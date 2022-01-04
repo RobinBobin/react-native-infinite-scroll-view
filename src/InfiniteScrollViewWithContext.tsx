78c1906c871fb55b4ede6631dd1f3df669479232
Ruben Shalimov <r_shalimov@inbox.ru>
2022-01-04 03:19:17 +0300

    1) 0.0.3. 2) "deep-equal": "^2.0.5". 3) npm update. 4) src/InfiniteScrollViewWithContext.tsx: React.FC + React.memo().

--- src/InfiniteScrollViewWithContext.tsx
+++ src/InfiniteScrollViewWithContext.tsx
@@ -1,3 +1,4 @@
+import deepEqual from "deep-equal";
 import React from "react";
 import { StyleSheet } from "react-native";
 import Context from "./Context";
@@ -5,15 +6,23 @@ import { ContextType } from "./types/context";
 import { BaseItemType } from "./types/data";
 import { InfiniteScrollView } from "./ui/InfiniteScrollView";
 
-export default function InfiniteScrollViewWithContext <ItemT extends BaseItemType> (props: ContextType <ItemT> ) {
-  return (
-    <Context.Provider
-      value={{
-        ...props,
-        style: StyleSheet.flatten(props.style)
-      }}
-    >
-      <InfiniteScrollView />
-    </Context.Provider>
-  );
+export default <ItemT extends BaseItemType> () => {
+  const InfiniteScrollViewWithContext: React.FC <ContextType <ItemT>> = (props) => {
+    console.log("InfiniteScrollViewWithContext rendered");
+    
+    return (
+      <Context.Provider
+        value={{
+          ...props,
+          style: StyleSheet.flatten(props.style)
+        }}
+      >
+        <InfiniteScrollView />
+      </Context.Provider>
+    );
+  };
+  
+  return React.memo(InfiniteScrollViewWithContext, (prev, next) => deepEqual(prev, next, {
+    strict: true
+  }));
 };
