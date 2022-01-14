378b900f40c1ef9d68172a9caeb2753dd3d584b5
Ruben Shalimov <r_shalimov@inbox.ru>
2022-01-14 22:57:19 +0300

    index.ts: fixes a bug in ea85c95a726252e2f280d94efa1ecedc78b984e7 (useDataHolder invalid import path).

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { default } from "./src/InfiniteScrollViewWithContext";
-export { useDataHolder } from "./src/data/DataHolder";
+export { useDataHolder } from "./src/data/DataHolderImpl";
 export { BaseItemType } from "./src/types/data";
 export { RenderItemInfo } from "./src/types/ui";
